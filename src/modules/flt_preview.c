35b4eac06ae6dcc058751de2b123e5ed7f2a1825
Christian Kruse <cjk@wwwtech.de>
2004-07-03 11:33:25 +0000

    see changelog
    git-svn-id: file:///Volumes/Daten/wwwtech/trunk@163 31cb981c-0fcd-0310-a313-f9fa081e70fd

---
+++ src/modules/flt_preview.c
@@ -0,0 +1,89 @@
+/*
+ * \file flt_preview.c
+ * \author Christian Kruse, <ckruse@wwwtech.de>
+ * \brief Posting preview
+ *
+ * This file is a plugin for fo_post. It gives the user the
+ * ability to preview his postings.
+ *
+ */
+
+/* {{{ Initial comments */
+/*
+ * $LastChangedDate$
+ * $LastChangedRevision$
+ * $LastChangedBy$
+ *
+ */
+/* }}} */
+
+/* {{{ Includes */
+#include "config.h"
+#include "defines.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include <time.h>
+
+#include "readline.h"
+#include "hashlib.h"
+#include "utils.h"
+#include "configparser.h"
+#include "cfcgi.h"
+#include "template.h"
+#include "clientlib.h"
+#include "fo_post.h"
+/* }}} */
+
+static u_char *flt_preview_datefmt = NULL;
+
+int flt_preview_execute(t_cf_hash *head,t_configuration *dc,t_configuration *pc,t_message *p,int sock,int mode) {
+  u_char *date;
+  size_t len;
+  t_name_value *v;
+
+  if(head) {
+    if(cf_cgi_get(head,"preview")) {
+      v = cfg_get_first_value(dc,"DateLocale");
+
+      date = general_get_time(flt_preview_datefmt,v->values[0],&len,&p->date);
+      _cf_cgi_save_param(head,"date",4,date);
+      free(date);
+    }
+  }
+
+  return FLT_DECLINE;
+}
+
+int flt_preview_cmd(t_configfile *cfile,t_conf_opt *opt,u_char **args,int argnum) {
+  if(flt_preview_datefmt) free(flt_preview_datefmt);
+  flt_preview_datefmt = strdup(args[0]);
+
+  return 0;
+}
+
+t_conf_opt flt_preview_config[] = {
+  { "PreviewDateFormat", flt_preview_cmd, NULL },
+  { NULL, NULL, NULL }
+};
+
+t_handler_config flt_preview_handlers[] = {
+  { NEW_POST_HANDLER, flt_preview_execute },
+  { 0, NULL }
+};
+
+t_module_config flt_preview = {
+  flt_preview_config,
+  flt_preview_handlers,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+
+/* eof */
