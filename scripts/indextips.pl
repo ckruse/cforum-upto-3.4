4ba5b79070e275ae5b6b5a8f2c563f505dcf73cf
Christian Kruse <cjk@wwwtech.de>
2005-01-17 02:44:44 +0000

    see changelog
    git-svn-id: file:///Volumes/Daten/wwwtech/cforum/trunk@457 31cb981c-0fcd-0310-a313-f9fa081e70fd

--- scripts/indextips.pl
+++ scripts/indextips.pl
@@ -2,8 +2,19 @@
 
 use strict;
 
-our $TIPS_FILE  = 'cgi-shared/tipoftheday.txt';
-our $INDEX_FILE = 'cgi-shared/tipoftheday.idx';
+use Getopt::Long;
+
+our $TIPS_FILE  = 'tipoftheday.txt';
+our $INDEX_FILE = 'tipoftheday.idx';
+our $help = 0;
+
+GetOptions(
+  "tips-file=s" => \$messagefile_in,
+  "tips-index=s" => \$messagefile_out,
+  "help" => \$help
+)
+
+usage() if $help;
 
 open DAT,'<',$TIPS_FILE or die "$TIPS_FILE: $!";
 
@@ -26,5 +37,21 @@ foreach my $offset (@lines) {
 }
 
 close IDX;
+
+sub usage {
+  print <<TXT;
+USAGE:
+  $0 [options]
+
+Options are:
+  --tips-file=file      Path to the tips file
+  --tips-index=file     Path to the tips index file
+  --help                Show this help screen
+
+TXT
+
+  exit;
+}
+
 
 # eof
---
+++ scripts/msgs2db.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl -w
+
+# {{{ initial comments
+#
+# $LastChangedDate$
+# $LastChangedRevision$
+# $LastChangedBy$
+#
+# }}}
+
+# {{{ program header
+use strict;
+
+use BerkeleyDB;
+use Getopt::Long;
+
+sub VERSION {(q$Revision$ =~ /([\d.]+)\s*$/)[0] or '0.0'}
+# }}}
+
+my $messagefile_in = '';
+my $messagefile_out = '';
+my $lang = '';
+my $help = 0;
+
+GetOptions(
+  "message-file=s" => \$messagefile_in,
+  "message-db=s" => \$messagefile_out,
+  "lang" => \$lang,
+  "help" => \$help
+);
+
+usage() if $help;
+
+my $db = new BerkeleyDB::Btree(
+  -Filename => $messagefile_out,
+	-Flags => DB_CREATE
+) or die "BDB error: $!";
+
+if(!$lang) {
+  if($msgfile !~ /\.([a-z]{2})\./) {
+    print STDERR "Could not get language! Please set by --lang!\n";
+    exit -1;
+  }
+
+  $lang = $1;
+}
+
+print "Working on $messagefile_in (language: $lang)\n";
+open DAT,'<',$messagefile_in or die "$messagefile_in: $!";
+
+while(<DAT>) {
+  next unless m/^([a-zA-Z0-9_]+): (.*)\n/;
+  my ($key,$value) = ($1,$2);
+  $db->db_put("${lang}_$key",$value);
+}
+
+close DAT;
+undef $db;
+
+sub usage {
+  print <<TXT;
+USAGE:
+  $0 [options]
+
+Options are:
+  --message-file=file   Path to the messages file
+  --message-db=file     Path to the message database
+  --lang=lang           Name of the language (e.g. de, en)
+  --help                Show this help screen
+
+TXT
+
+  exit -1;
+}
+
+# eof
