5892fbbe0317522fc27951a379ca008207530173
Christian Kruse <cjk@wwwtech.de>
2005-02-21 15:02:51 +0000

    merge from 3.0 branch
    git-svn-id: file:///Volumes/Daten/wwwtech/cforum/trunk@663 31cb981c-0fcd-0310-a313-f9fa081e70fd

---
+++ src/perl/Plugins/Pathes.pm
@@ -0,0 +1,43 @@
+package Plugins::Pathes;
+
+#
+# \file CheckRegisteredName.pm
+# \author Christian Kruse, <ckruse@wwwtech.de>
+#
+# a plugin to register usernames
+#
+
+# {{{ initial comments
+#
+# $LastChangedDate$
+# $LastChangedRevision$
+# $LastChangedBy$
+#
+# }}}
+
+# {{{ plugin header
+use strict;
+
+sub VERSION {(q$Revision$ =~ /([\d.]+)\s*$/)[0] or '0.0'}
+
+use IO::Socket;
+
+push @{$main::Plugins->{register}},\&execute;
+# }}}
+
+sub execute {
+  my $fo_default_conf = shift;
+  my $fo_usermanagement_conf = shift;
+  my $user_config = shift;
+  my $uconf_dir = shift;
+  my $uconf_file = shift;
+  my $fh = shift;
+
+  print $fh 'DeletedFile "',$uconf_dir,"/dt.dat\"\n";
+  print $fh 'VisitedFile "',$uconf_dir,"/vt.dat\"\n";
+  print $fh 'MailUserDB "',$uconf_dir,"/ml.dat\"\n";
+}
+
+
+1;
+# eof
