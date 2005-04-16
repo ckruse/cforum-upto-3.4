2ad468f5710952d585b50a3db6ea17cfebfd9cb0
Christian Kruse <cjk@wwwtech.de>
2005-04-16 12:21:52 +0000

    see changelog
    git-svn-id: file:///Volumes/Daten/wwwtech/cforum/trunk@850 31cb981c-0fcd-0310-a313-f9fa081e70fd

--- src/modules/flt_admin.c
+++ src/modules/flt_admin.c
@@ -36,8 +36,12 @@
 
 u_char **flt_admin_Admins = NULL;
 static size_t flt_admin_AdminNum = 0;
-static int my_errno = 0;
-static int is_admin = -1;
+static int my_errno      = 0;
+static int is_admin      = -1;
+static int flt_admin_204 = 0;
+static int flt_admin_js  = 0;
+
+static u_char *flt_admin_fn = NULL;
 
 /* {{{ flt_admin_is_admin */
 int flt_admin_is_admin(const u_char *name) {
@@ -70,12 +74,21 @@ int flt_admin_gogogo(t_cf_hash *cgi,t_configuration *dc,t_configuration *vc,void
   #ifdef CF_SHARED_MEM
   int sock;
   #endif
-  u_char *action = NULL,*tid,*mid,buff[512],*answer;
+  u_char *action = NULL,*tid,*mid,buff[512],*answer,*mode;
   size_t len;
   rline_t rl;
-  int x;
+  int x,ret;
+
+  u_int64_t itid;
+
   u_char *UserName = cf_hash_get(GlobalValues,"UserName",8);
   u_char *forum_name = cf_hash_get(GlobalValues,"FORUM_NAME",10);
+  t_name_value *cs,*ot,*ct;;
+
+  cf_readmode_t *rm;
+
+  t_cl_thread thread;
+  t_string str;
 
   if(!flt_admin_is_admin(UserName)) return FLT_DECLINE;
 
@@ -118,13 +131,68 @@ int flt_admin_gogogo(t_cf_hash *cgi,t_configuration *dc,t_configuration *vc,void
     if(answer) free(answer);
 
     #ifdef CF_SHARED_MEM
-    writen(sock,"QUIT\n",5);
-    close(sock);
-    cf_reget_shm_ptr();
+    ptr = cf_reget_shm_ptr();
     #endif
+
+    itid = str_to_u_int64(tid);
 
     cf_hash_entry_delete(cgi,"t",1);
     cf_hash_entry_delete(cgi,"m",1);
+
+    if((mode = cf_cgi_get(cgi,"mode")) != NULL && cf_strcmp(mode,"xmlhttp") == 0) {
+      cs = cfg_get_first_value(&fo_default_conf,forum_name,"ExternCharset");
+
+      if(cf_strcmp(mode,"archive") == 0) printf("Content-Type: text/html; charset=%s\015\012\015\012Ok\n",cs->values[0]);
+      else {
+        /* {{{ init variables */
+        cs = cfg_get_first_value(dc,forum_name,"ExternCharset");
+        ot = cfg_get_first_value(vc,forum_name,"OpenThread");
+        ct = cfg_get_first_value(vc,forum_name,"CloseThread");
+
+        rm = cf_hash_get(GlobalValues,"RM",2);
+
+        memset(&thread,0,sizeof(thread));
+        memset(&rl,0,sizeof(rl));
+        /* }}} */
+
+        if(cf_get_message_through_sock(sock,&rl,&thread,rm->threadlist_thread_tpl,itid,0,CF_KEEP_DELETED) == -1) fprintf(stderr,"500 Internal Server Error\015\012\015\012Error");
+
+        thread.threadmsg = thread.messages;
+
+        #ifndef CF_NO_SORTING
+        #ifdef CF_SHARED_MEM
+        cf_run_thread_sorting_handlers(cgi,ptr,&thread);
+        #else
+        cf_run_thread_sorting_handlers(cgi,sock,&rl,&thread);
+        #endif
+        #endif
+
+        str_init(&str);
+        cf_gen_threadlist(&thread,cgi,&str,"full",NULL,CF_MODE_THREADLIST);
+        cf_cleanup_thread(&thread);
+
+        printf("Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
+        fwrite(str.content + strlen(ot->values[0]),1,str.len - strlen(ot->values[0]) - strlen(ct->values[0]),stdout);
+        str_cleanup(&str);
+      }
+
+      #ifdef CF_SHARED_MEM
+      writen(sock,"QUIT\n",5);
+      close(sock);
+      #endif
+
+      return FLT_EXIT;
+    }
+
+    #ifdef CF_SHARED_MEM
+    writen(sock,"QUIT\n",5);
+    close(sock);
+    #endif
+
+    if(flt_admin_204) {
+      printf("Status: 204 No Content\015\012\015\012");
+      return FLT_EXIT;
+    }
 
     return FLT_OK;
   }
@@ -143,7 +211,10 @@ int flt_admin_setvars(t_cf_hash *cgi,t_configuration *dc,t_configuration *vc,t_c
   if(flt_admin_is_admin(UserName)) {
     cf_tpl_setvalue(top,"admin",TPL_VARIABLE_INT,1);
 
-    if(ShowInvisible) cf_tpl_setvalue(top,"aaf",TPL_VARIABLE_INT,1);
+    if(ShowInvisible) {
+      cf_tpl_setvalue(top,"aaf",TPL_VARIABLE_INT,1);
+      if(flt_admin_js) cf_tpl_setvalue(top,"AdminJS",TPL_VARIABLE_INT,1);
+    }
 
     if(my_errno) {
       len = snprintf(buff,256,"E_FO_%d",my_errno);
@@ -167,8 +238,9 @@ int flt_admin_posting_setvars(t_cf_hash *head,t_configuration *dc,t_configuratio
   int ShowInvisible = cf_hash_get(GlobalValues,"ShowInvisible",13) != NULL;
 
   if(flt_admin_is_admin(UserName)) {
-    cf_tpl_setvalue(tpl,"admin",TPL_VARIABLE_STRING,"1",1);
-    if(ShowInvisible) cf_tpl_setvalue(tpl,"aaf",TPL_VARIABLE_STRING,"1",1);
+    cf_tpl_setvalue(tpl,"admin",TPL_VARIABLE_INT,1);
+
+    if(ShowInvisible) cf_tpl_setvalue(tpl,"aaf",TPL_VARIABLE_INT,1);
 
     return FLT_OK;
   }
@@ -243,10 +315,11 @@ int flt_admin_posthandler(t_cf_hash *cgi,t_configuration *dc,t_configuration *vc
   if(!UserName) return FLT_DECLINE;
 
   if(flt_admin_is_admin(UserName) && ShowInvisible) {
-    cf_tpl_setvalue(&msg->tpl,"admin",TPL_VARIABLE_STRING,"1",1);
+    cf_tpl_setvalue(&msg->tpl,"admin",TPL_VARIABLE_INT,1);
 
     if(ShowInvisible) {
-      cf_tpl_setvalue(&msg->tpl,"aaf",TPL_VARIABLE_STRING,"1",1);
+      cf_tpl_setvalue(&msg->tpl,"aaf",TPL_VARIABLE_INT,1);
+      if(flt_admin_js && (mode & CF_MODE_THREADLIST)) cf_tpl_setvalue(&msg->tpl,"AdminJS",TPL_VARIABLE_INT,1);
 
       link = cf_advanced_get_link(rm->posting_uri[1],tid,msg->mid,NULL,1,&l,"faa","archive");
       cf_tpl_setvalue(&msg->tpl,"archive_link",TPL_VARIABLE_STRING,link,l);
@@ -312,7 +385,21 @@ time_t flt_admin_lm(t_cf_hash *head,t_configuration *dc,t_configuration *vc,void
 }
 /* }}} */
 
+/* {{{ flt_admin_handle */
+int flt_admin_handle(t_configfile *cfile,t_conf_opt *opt,const u_char *context,u_char **args,size_t argnum) {
+  if(flt_admin_fn == NULL) flt_admin_fn = cf_hash_get(GlobalValues,"FORUM_NAME",10);
+  if(!context || cf_strcmp(flt_admin_fn,context) != 0) return 0;
+
+  if(cf_strcmp(opt->name,"AdminSend204") == 0) flt_admin_204 = cf_strcmp(args[0],"yes") == 0;
+  else if(cf_strcmp(opt->name,"AdminUseJS") == 0) flt_admin_js = cf_strcmp(args[0],"yes") == 0;
+
+  return 0;
+}
+/* }}} */
+
 t_conf_opt flt_admin_config[] = {
+  { "AdminSend204", flt_admin_handle, CFG_OPT_USER|CFG_OPT_LOCAL, NULL },
+  { "AdminUseJS",   flt_admin_handle, CFG_OPT_USER|CFG_OPT_LOCAL, NULL },
   { NULL, NULL, 0, NULL }
 };
 
