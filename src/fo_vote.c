3a3e0cfceb931916b765fb4656fedd901eff8ec0
Christian Kruse <cjk@wwwtech.de>
2005-05-27 09:40:03 +0000

    see changelog
    git-svn-id: file:///Volumes/Daten/wwwtech/cforum/trunk@1062 31cb981c-0fcd-0310-a313-f9fa081e70fd

--- src/fo_vote.c
+++ src/fo_vote.c
@@ -321,13 +321,15 @@ int main(int argc,char *argv[],char *env[]) {
 
         if((ret = db->get(db,NULL,&key,&data,0)) == 0) {
           printf("Status: 403 Forbidden\015\012Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
-          cf_error_message("E_VOTE_MULTIPLE",NULL);
+          if(mode && cf_strcmp(mode,"xmlhttp") == 0) cf_error_message("E_VOTE_MULTIPLE",NULL);
+          else printf("0\n");
           return EXIT_FAILURE;
         }
 
         if(ret != DB_NOTFOUND) {
           printf("Status: 500 Internal Server Error\015\012Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
-          cf_error_message("E_VOTE_INTERNAL",NULL);
+          if(mode && cf_strcmp(mode,"xmlhttp") == 0) cf_error_message("E_VOTE_INTERNAL",NULL);
+          else printf("0\n");
           fprintf(stderr,"fo_vote: db->get() error: %s\n",db_strerror(ret));
           return EXIT_FAILURE;
         }
@@ -337,7 +339,8 @@ int main(int argc,char *argv[],char *env[]) {
 
         if((ret = db->put(db,NULL,&key,&data,0)) != 0) {
           printf("Status: 500 Internal Server Error\015\012Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
-          cf_error_message("E_VOTE_INTERNAL",NULL);
+          if(mode && cf_strcmp(mode,"xmlhttp") == 0) cf_error_message("E_VOTE_INTERNAL",NULL);
+          else printf("0\n");
           fprintf(stderr,"fo_vote: db->put() error: %s\n",db_strerror(ret));
           return EXIT_FAILURE;
         }
@@ -394,17 +397,20 @@ int main(int argc,char *argv[],char *env[]) {
       else {
         printf("Status: 500 Internal Server Error\015\012Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
         perror("socket");
-        cf_error_message("E_VOTE_INTERNAL",NULL);
+        if(mode && cf_strcmp(mode,"xmlhttp") == 0) cf_error_message("E_VOTE_INTERNAL",NULL);
+        else printf("0\n");
       }
     }
     else {
       printf("Status: 500 Internal Server Error\015\012Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
-      cf_error_message("E_VOTE_INTERNAL",NULL);
+      if(mode && cf_strcmp(mode,"xmlhttp") == 0) cf_error_message("E_VOTE_INTERNAL",NULL);
+      else printf("0\n");
     }
   }
   else {
     printf("Status: 500 Internal Server Error\015\012Content-Type: text/html; charset=%s\015\012\015\012",cs->values[0]);
-    cf_error_message("E_VOTE_INTERNAL",NULL);
+    if(mode && cf_strcmp(mode,"xmlhttp") == 0) cf_error_message("E_VOTE_INTERNAL",NULL);
+    else printf("0\n");
   }
 
 
