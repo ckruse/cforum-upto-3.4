ae7ec1e0a852617ab155eff7fade51ef248def5a
Christian Kruse <cjk@wwwtech.de>
2009-01-14 20:06:58 +0000

    added script to update signature tokens
    git-svn-id: file:///Volumes/Daten/wwwtech/cforum/branches/3.4@1630 31cb981c-0fcd-0310-a313-f9fa081e70fd

---
+++ scripts/update_sighandling.sh
@@ -0,0 +1,10 @@
+
+dir="$@"
+if test -z "$dir"; then
+  dir="./";
+fi
+
+echo "Working on $dir..."
+find "$dir" -name "t*.xml" -print0 | xargs -0 perl -pi -e 's!_/_SIG_/_!<br />-- <br />!g'
+echo "done!"
+
